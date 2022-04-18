e04287a86fb684c3485f2e406c5de8a84a09e6de
onlyyangfan <onlyyangfan@protonmail.com>
2022-04-18 16:53:36 +0800

    4.18

--- ServerCpp/echoserver/echoclient.cpp
+++ ServerCpp/echoserver/echoclient.cpp
@@ -8,10 +8,37 @@
  */
 
 /* @#define NETWORK to get network lib   @#define C_LIB to get c lib */
-#include "/home/yangfan/HeadCPP.hpp"
+#include <iostream>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <cstring>
+#include <arpa/inet.h>
+
 
 using namespace std;
 
 int main(int argc, char **argv) {
-	cout << "client\n";
+	struct sockaddr_in serv_addr;
+	int clnt_sock = socket(AF_INET, SOCK_STREAM, 0);
+	
+	bzero(&serv_addr, sizeof(serv_addr));
+	
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(atoi(argv[2]));
+
+	socklen_t addr_sz = sizeof(serv_addr);
+	if ((connect(clnt_sock, reinterpret_cast<struct sockaddr*>(&serv_addr), addr_sz)) == -1) {
+		perror("connect");
+		exit(EXIT_FAILURE);
+	}
+
+	cout << "connect to server: " << inet_ntoa(serv_addr.sin_addr) << '\n';
+
+	char buf[1024] = {"hello world"};
+	write(clnt_sock, buf, strlen(buf));
+	read(clnt_sock, buf, sizeof(buf));
+	cout << buf << "\n";
+	close(clnt_sock);
+
 }
--- ServerCpp/echoserver/echoserver.cpp
+++ ServerCpp/echoserver/echoserver.cpp
@@ -12,22 +12,49 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <cstring>
+#include <netinet/in.h>
 
+int port = 8888;
 
 
 using namespace std;
 
 int main(int argc, char **argv) {
-	struct sockaddr_in serv_addr;
-
+	struct sockaddr_in serv_addr, clnt_addr;
+	int clnt_sock;
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0); // create socket fd
 	
 	bzero(&serv_addr, sizeof(serv_addr)); // init address struct
 
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	serv_addr.sin_port = htons(8888);
+	serv_addr.sin_port = htons(port);
 
-	bind(sockfd, static_cast<sockaddr*>(serv_addr), sizeof(serv_addr));
+	if ((bind(sockfd, reinterpret_cast<struct sockaddr*>(&serv_addr), sizeof(serv_addr))) == -1) {
+		perror("server bind");
+		exit(EXIT_FAILURE);
+	}
+
+	if ((listen(sockfd, SOMAXCONN)) == -1) {
+		perror("server listen");
+		exit(EXIT_FAILURE);
+	}
+
+	cout << "Server start........\nIP: " << inet_ntoa(serv_addr.sin_addr) << '\n' << "Port: " << port << '\n';
+	for (auto i = 0; i < SOMAXCONN; i++) {
+		char buf[1024];
+		socklen_t addr_sz = sizeof(clnt_addr);
+		clnt_sock = accept(sockfd, reinterpret_cast<struct sockaddr*>(&clnt_addr), &addr_sz); // accept a new client socket
+		cout << "new client socket is connected...\nConnect client IP: " << inet_ntoa(clnt_addr.sin_addr) << '\n';
+		while (read(clnt_sock, buf, 1024) != 0) {
+			cout << "rcv: " << buf << '\n';
+			write(clnt_sock, buf, strlen(buf));
+		}
+
+		close(clnt_sock);
+	}
+	close(sockfd);
+
+
 
 }
